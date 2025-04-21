b85bba6cc5f991762c812b1c188ffbc95537d764
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2025-04-21 10:20:42 +1000

    enemy types

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -4,20 +4,25 @@ extends Node
 @onready var ground_1: TileMap = $"../Ground1"
 @onready var ground_2: TileMap = $"../Ground2"
 
-var SPEED = 1
+var SPEED = 60
 var DIRECTION = -1
-var VELOCITY = -1
+var VELOCITY = -60
 var GROUND_WIDTH
-# Called when the node enters the scene tree for the first time.
+
+#var rocket_scene = preload("res://scenes/enemy.tscn")
+#var enemies_type := [rocket_scene, ...]
+
 func _ready() -> void:
 	ground_1.position.x = 0
 	GROUND_WIDTH = ground_1.get_used_rect().size.x * ground_1.tile_set.tile_size.x
 	ground_2.position.x = GROUND_WIDTH
+	VELOCITY = SPEED * DIRECTION
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	ground_1.position.x += VELOCITY
-	ground_2.position.x += VELOCITY
+	var res = VELOCITY * delta
+	ground_1.position.x += res
+	ground_2.position.x += res
 	if ground_1.position.x < -GROUND_WIDTH:
 		ground_1.position.x = ground_2.position.x + GROUND_WIDTH
 	if ground_2.position.x < -GROUND_WIDTH:
@@ -27,4 +32,17 @@ func get_speed() -> int:
 	return SPEED
 	
 func get_velocity() -> int:
-	return SPEED * DIRECTION
+	return VELOCITY
+
+func spawn_enemy() -> void:
+	pass
+	#var enemy_type = enemies_type[randi() % enemies_type.size()]
+	#var newEnemy
+	#newEnemy = enemy_type.instantiate()
+	#append some shit
+
+func despawn_enemy(enemy: Enemy) -> void:
+	pass
+	enemy.queue_free()
+	#remove from array shit
+	
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,8 +1,17 @@
 extends Area2D
 class_name Enemy
-
-@onready var game_manager: Node = %GameManager
+#right click on enemy.tscn then New Inherited Scene
+@onready var game_manager: Node = get_tree().root.get_node("Game/GameManager")
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var VELOCITY
+
+func _ready() -> void:
+	VELOCITY = game_manager.get_velocity()
+	
 func _process(delta: float) -> void:
-	position.x += game_manager.get_velocity() * delta
+	position.x += VELOCITY * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	print("DEATHSDSDMSDJNSDJ")
+	queue_free
